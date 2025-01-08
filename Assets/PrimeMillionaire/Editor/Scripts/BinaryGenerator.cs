08ca09bf8347a6da1436c48daf45499f890bb02c
kitatas <315kitatas10703@gmail.com>
2025-01-08 23:06:40 +0900

    fix: Binary生成

--- Assets/PrimeMillionaire/Editor/Scripts/BinaryGenerator.cs
+++ Assets/PrimeMillionaire/Editor/Scripts/BinaryGenerator.cs
@@ -23,28 +23,10 @@ namespace PrimeMillionaire.Editor.Scripts
             var options = MessagePackSerializerOptions.Standard.WithResolver(messagePackResolvers);
             MessagePackSerializer.DefaultOptions = options;
 
-            var cardMaster = new List<CardMaster>();
-            var id = 1;
-            foreach (var suit in CardConfig.SUITS)
-            {
-                for (int i = 1; i <= CardConfig.MAX_RANK; i++)
-                {
-                    var imgPath = $"Assets/Externals/Sprites/Cards/card_{suit.FastToString().ToLower()}s_{i}.png";
-                    cardMaster.Add(new CardMaster(id++, suit, i, imgPath));
-                }
-            }
-            
-            var characterMaster = new List<CharacterMaster>();
-            foreach (var type in FastEnum.GetValues<CharacterType>())
-            {
-                if (type == CharacterType.None) continue;
-                var objPath = $"Assets/PrimeMillionaire/Prefabs/Characters/{type.FastToString()}.prefab";
-                characterMaster.Add(new CharacterMaster(type.ToInt32(), objPath));
-            }
-
             var databaseBuilder = new DatabaseBuilder();
-            databaseBuilder.Append(cardMaster);
-            databaseBuilder.Append(characterMaster);
+            databaseBuilder.Append(GetCardMaster());
+            databaseBuilder.Append(GetCharacterMaster());
+            databaseBuilder.Append(GetParameterMaster());
             var binary = databaseBuilder.Build();
 
             var bytes = "Assets/Externals/Binary/MasterMemory.bytes";
@@ -56,6 +38,43 @@ namespace PrimeMillionaire.Editor.Scripts
 
             File.WriteAllBytes(bytes, binary);
             AssetDatabase.Refresh();
+        }
+
+        private static List<CardMaster> GetCardMaster()
+        {
+            var cardMaster = new List<CardMaster>();
+            var id = 1;
+            foreach (var suit in CardConfig.SUITS)
+            {
+                for (int i = 1; i <= CardConfig.MAX_RANK; i++)
+                {
+                    var imgPath = $"Assets/Externals/Sprites/Cards/card_{suit.FastToString().ToLower()}s_{i}.png";
+                    cardMaster.Add(new CardMaster(id++, suit, i, imgPath));
+                }
+            }
+
+            return cardMaster;
+        }
+
+        private static List<CharacterMaster> GetCharacterMaster()
+        {
+            var characterMaster = new List<CharacterMaster>();
+            foreach (var type in FastEnum.GetValues<CharacterType>())
+            {
+                if (type == CharacterType.None) continue;
+                var objPath = $"Assets/PrimeMillionaire/Prefabs/Characters/{type.FastToString()}.prefab";
+                characterMaster.Add(new CharacterMaster(type.ToInt32(), objPath));
+            }
+
+            return characterMaster;
+        }
+
+        private static List<ParameterMaster> GetParameterMaster()
+        {
+            var parameterMaster = new List<ParameterMaster>();
+            parameterMaster.Add(new ParameterMaster(CharacterType.Andromeda.ToInt32(), 1100, 110, 120));
+            parameterMaster.Add(new ParameterMaster(CharacterType.Borealjuggernaut.ToInt32(), 1200, 130, 140));
+            return parameterMaster;
         }
     }
 }
