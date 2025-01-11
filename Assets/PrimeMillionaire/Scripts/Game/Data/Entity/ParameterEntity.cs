284ade2b94fabe130ea17ac663501e0a2ad8be91
kitatas <315kitatas10703@gmail.com>
2025-01-11 22:57:16 +0900

    fix: ダメージ計算

--- Assets/PrimeMillionaire/Scripts/Game/Data/Entity/ParameterEntity.cs
+++ Assets/PrimeMillionaire/Scripts/Game/Data/Entity/ParameterEntity.cs
@@ -22,6 +22,6 @@ namespace PrimeMillionaire.Game.Data.Entity
         public void SetHp(int hp) => _hp = hp;
 
         public void Heal(int value) => _hp = Mathf.Min(maxHp, currentHp + value);
-        public void Damage(int value) => _hp = Mathf.Max(0, currentHp - value);
+        public void Damage(int value) => _hp = Mathf.Max(0, currentHp - Mathf.Max(0, value));
     }
 }
