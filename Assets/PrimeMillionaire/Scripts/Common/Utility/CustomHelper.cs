44d9836aa8314793dc8fd3b5e96b0fef7373adce
kitatas <315kitatas10703@gmail.com>
2025-01-19 22:21:42 +0900

    fix: CustomHelper

--- Assets/PrimeMillionaire/Scripts/Common/Utility/CustomHelper.cs
+++ Assets/PrimeMillionaire/Scripts/Common/Utility/CustomHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine.AddressableAssets;
@@ -9,6 +10,14 @@ namespace PrimeMillionaire.Common.Utility
         public static async UniTask<T> LoadAsync<T>(string path, CancellationToken token) where T : UnityEngine.Object
         {
             return await Addressables.LoadAssetAsync<T>(path).WithCancellation(token);
+        }
+    }
+
+    public static class UniTaskHelper
+    {
+        public static UniTask DelayAsync(float duration, CancellationToken token)
+        {
+            return UniTask.Delay(TimeSpan.FromSeconds(duration), cancellationToken: token);
         }
     }
 }
