e9de199d9190ae06d16ace91cc77b05a3643d290
kitatas <315kitatas10703@gmail.com>
2025-02-23 22:46:20 +0900

    fix: Deck取得

--- Assets/PrimeMillionaire/Scripts/Common/Domain/Repository/DeckRepository.cs
+++ Assets/PrimeMillionaire/Scripts/Common/Domain/Repository/DeckRepository.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using FastEnumUtility;
 using PrimeMillionaire.Common.Data.DataStore;
-using PrimeMillionaire.Common.Utility;
-using UniEx;
 
 namespace PrimeMillionaire.Common.Domain.Repository
 {
@@ -18,22 +17,16 @@ namespace PrimeMillionaire.Common.Domain.Repository
 
         public IEnumerable<CardVO> GetCards(CharacterType type)
         {
-            var master = _memoryDatabase.CardMasterTable.All;
-            var cards = type switch
+            if (_memoryDatabase.DeckMasterTable.TryFindByType(type.ToInt32(), out var deck))
             {
-                // TODO: ベタ書き
-                CharacterType.Andromeda => master.Where(x => x.Suit.ToSuit() is Suit.Spade or Suit.Heart),
-                CharacterType.Borealjuggernaut => master.Where(x => !x.Rank.IsEven()),
-                CharacterType.Dissonance => master.Where(x => x.Rank >= 7),
-                CharacterType.Kron => master.Where(x => x.Rank <= 7),
-                CharacterType.Paragon => master,
-                CharacterType.Harmony => master.Where(x => x.Rank is 1 or 2 or 3 or 5 or 8 or 13),
-                CharacterType.Candypanda => master.Where(x => x.Rank is 4 or 6 or 7 or 9 or 10 or 11 or 12),
-                CharacterType.Chaosknight => master.Where(x => x.Rank is 2 or 3 or 5 or 7 or 11),
-                _ => throw new Exception(),
-            };
-
-            return cards.Select(x => x.ToVO());
+                return _memoryDatabase.CardMasterTable.All
+                    .Where(x => deck.Suits.Contains(x.Suit) && deck.Ranks.Contains(x.Rank))
+                    .Select(x => x.ToVO());
+            }
+            else
+            {
+                throw new Exception();
+            }
         }
     }
 }
