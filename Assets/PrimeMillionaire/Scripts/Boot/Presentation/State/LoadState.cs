ef3d8024772676a59f89c776161c590c354e94cf
kitatas <315kitatas10703@gmail.com>
2025-03-06 00:39:54 +0900

    fix: LoadState

--- Assets/PrimeMillionaire/Scripts/Boot/Presentation/State/LoadState.cs
+++ Assets/PrimeMillionaire/Scripts/Boot/Presentation/State/LoadState.cs
@@ -1,35 +1,30 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
-using PrimeMillionaire.Boot.Presentation.View;
 using PrimeMillionaire.Common;
 using PrimeMillionaire.Common.Domain.UseCase;
-using R3;
 
 namespace PrimeMillionaire.Boot.Presentation.State
 {
     public sealed class LoadState : BaseState
     {
         private readonly SceneUseCase _sceneUseCase;
-        private readonly TitleView _titleView;
 
-        public LoadState(SceneUseCase sceneUseCase, TitleView titleView)
+        public LoadState(SceneUseCase sceneUseCase)
         {
             _sceneUseCase = sceneUseCase;
-            _titleView = titleView;
         }
 
         public override BootState state => BootState.Load;
 
         public override async UniTask InitAsync(CancellationToken token)
         {
-            _titleView.Init();
             await UniTask.Yield(token);
         }
 
         public override async UniTask<BootState> TickAsync(CancellationToken token)
         {
-            await _titleView.push.FirstAsync(cancellationToken: token);
             _sceneUseCase.Load(SceneName.Top, LoadType.Fade);
+            await UniTask.Yield(token);
             return BootState.None;
         }
     }
--- Assets/PrimeMillionaire/Scripts/Boot/Presentation/State/LoginState.cs
+++ Assets/PrimeMillionaire/Scripts/Boot/Presentation/State/LoginState.cs
@@ -2,27 +2,36 @@ using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using PrimeMillionaire.Boot.Domain.UseCase;
+using PrimeMillionaire.Boot.Presentation.View;
+using PrimeMillionaire.Common.Utility;
+using R3;
 
 namespace PrimeMillionaire.Boot.Presentation.State
 {
     public sealed class LoginState : BaseState
     {
         private readonly LoginUseCase _loginUseCase;
+        private readonly TitleView _titleView;
 
-        public LoginState(LoginUseCase loginUseCase)
+        public LoginState(LoginUseCase loginUseCase, TitleView titleView)
         {
             _loginUseCase = loginUseCase;
+            _titleView = titleView;
         }
 
         public override BootState state => BootState.Login;
 
         public override async UniTask InitAsync(CancellationToken token)
         {
+            _titleView.Init();
             await UniTask.Yield(token);
         }
 
         public override async UniTask<BootState> TickAsync(CancellationToken token)
         {
+            // 他presenter初期化待ち
+            await UniTaskHelper.DelayAsync(0.5f, token);
+
             var (isSuccess, hasInterrupt) = _loginUseCase.Login();
             if (isSuccess == false)
             {
@@ -34,7 +43,7 @@ namespace PrimeMillionaire.Boot.Presentation.State
                 return BootState.Interrupt;
             }
 
-            await UniTask.Yield(token);
+            await _titleView.push.FirstAsync(cancellationToken: token);
             return BootState.Load;
         }
     }
