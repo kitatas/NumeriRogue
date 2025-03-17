5a72e48141579dbcd8e8df39aa9cca833927901b
kitatas <315kitatas10703@gmail.com>
2025-03-17 21:12:58 +0900

    add: Modal非表示時にScroll位置リセット

--- Assets/PrimeMillionaire/Scripts/Top/Presentation/View/Modal/TopModalView.cs
+++ Assets/PrimeMillionaire/Scripts/Top/Presentation/View/Modal/TopModalView.cs
@@ -1,12 +1,21 @@
+using DG.Tweening;
 using PrimeMillionaire.Common.Presentation.View.Modal;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace PrimeMillionaire.Top.Presentation.View.Modal
 {
     public sealed class TopModalView : BaseModalView
     {
         [SerializeField] private ModalType modalType = default;
+        [SerializeField] private ScrollRect scrollRect = default;
 
         public ModalType type => modalType;
+
+        public override Tween Hide(float duration)
+        {
+            return base.Hide(duration)
+                .OnComplete(() => scrollRect.verticalNormalizedPosition = 1.0f);
+        }
     }
 }
