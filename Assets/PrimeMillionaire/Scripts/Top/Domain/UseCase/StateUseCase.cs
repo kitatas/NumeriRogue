9557b13e031a5995df874db9c90f9b6910dc5e4c
kitatas <315kitatas10703@gmail.com>
2025-03-23 16:37:40 +0900

    fix: Top内のRetry処理

--- Assets/PrimeMillionaire/Scripts/Top/Domain/UseCase/StateUseCase.cs
+++ Assets/PrimeMillionaire/Scripts/Top/Domain/UseCase/StateUseCase.cs
@@ -1,20 +1,29 @@
 using System;
+using PrimeMillionaire.Common.Data.Entity;
 using R3;
 
 namespace PrimeMillionaire.Top.Domain.UseCase
 {
     public sealed class StateUseCase : IDisposable
     {
-        private readonly Subject<TopState> _state;
+        private readonly RetryCountEntity _retryCountEntity;
+        private readonly BehaviorSubject<TopState> _state;
 
-        public StateUseCase()
+        public StateUseCase(RetryCountEntity retryCountEntity)
         {
-            _state = new Subject<TopState>();
+            _retryCountEntity = retryCountEntity;
+            _state = new BehaviorSubject<TopState>(TopState.None);
         }
 
         public Observable<TopState> state => _state.Where(x => x != TopState.None);
 
         public void Set(TopState value) => _state?.OnNext(value);
+
+        public bool IsMaxRetry(TopState value)
+        {
+            _retryCountEntity.Update(_state.Value == value);
+            return _retryCountEntity.IsMaxRetry();
+        }
 
         public void Dispose()
         {
