af3cacc63e3a965e9285a7bdf658e6b45e76173b
kitatas <315kitatas10703@gmail.com>
2025-04-28 12:25:23 +0900

    add: Loading Animation

--- Assets/PrimeMillionaire/Scripts/Common/Presentation/View/LoadingView.cs
+++ Assets/PrimeMillionaire/Scripts/Common/Presentation/View/LoadingView.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -7,9 +8,47 @@ namespace PrimeMillionaire.Common.Presentation.View
     {
         [SerializeField] private TextMeshProUGUI loading = default;
 
+        private Sequence _sequence;
+
+        private void Awake()
+        {
+            _sequence = DOTween.Sequence();
+            var animator = new DOTweenTMPAnimator(loading);
+            var offset = Vector3.up * 10.0f;
+            var length = animator.textInfo.characterCount;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (char.IsWhiteSpace(loading.text[i])) continue;
+
+                var interval = (i + 1) * 0.05f;
+                _sequence
+                    .Join(DOTween.Sequence()
+                        .AppendInterval(0.05f)
+                        .Append(animator
+                            .DOOffsetChar(i, animator.GetCharOffset(i) + offset, 0.1f)
+                            .SetLoops(2, LoopType.Yoyo)
+                            .SetDelay(interval))
+                        .AppendInterval(length * 0.05f - interval));
+            }
+
+            _sequence
+                .SetLoops(-1)
+                .SetLink(loading.gameObject);
+        }
+
         public void Activate(bool value)
         {
             loading.enabled = value;
+
+            if (value)
+            {
+                _sequence?.Restart();
+            }
+            else
+            {
+                _sequence?.Pause();
+            }
         }
     }
 }
