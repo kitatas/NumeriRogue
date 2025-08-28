29f6baf5230eddab669212b652e5367a416502aa
kitatas <315kitatas10703@gmail.com>
2025-08-29 01:15:44 +0900

    add: Top SceneでのBGM再生

--- Assets/PrimeMillionaire/Scripts/Top/Presentation/State/InitState.cs
+++ Assets/PrimeMillionaire/Scripts/Top/Presentation/State/InitState.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using PrimeMillionaire.Common;
 using PrimeMillionaire.Common.Domain.UseCase;
 
 namespace PrimeMillionaire.Top.Presentation.State
@@ -7,10 +8,12 @@ namespace PrimeMillionaire.Top.Presentation.State
     public sealed class InitState : BaseState
     {
         private readonly LoadingUseCase _loadingUseCase;
+        private readonly SoundUseCase _soundUseCase;
 
-        public InitState(LoadingUseCase loadingUseCase)
+        public InitState(LoadingUseCase loadingUseCase, SoundUseCase soundUseCase)
         {
             _loadingUseCase = loadingUseCase;
+            _soundUseCase = soundUseCase;
         }
 
         public override TopState state => TopState.Init;
@@ -24,6 +27,8 @@ namespace PrimeMillionaire.Top.Presentation.State
         {
             await UniTask.Yield(token);
             _loadingUseCase.Set(false);
+
+            _soundUseCase.Play(Bgm.Menu);
 
             return TopState.Order;
         }
