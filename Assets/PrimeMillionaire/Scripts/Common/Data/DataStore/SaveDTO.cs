7d05887948f2ae1d2dc01f6a41ab1294414f2c86
kitatas <315kitatas10703@gmail.com>
2025-10-23 00:26:51 +0900

    fix: SaveDTOからProgressDTOを削除

--- Assets/PrimeMillionaire/Scripts/Common/Data/DataStore/SaveDTO.cs
+++ Assets/PrimeMillionaire/Scripts/Common/Data/DataStore/SaveDTO.cs
@@ -3,14 +3,12 @@ namespace PrimeMillionaire.Common.Data.DataStore
     public sealed class SaveDTO
     {
         public string uid;
-        public ProgressDTO progress;
         public InterruptDTO interrupt;
         public SoundDTO sound;
 
         public SaveDTO()
         {
             this.uid = "";
-            this.progress = new ProgressDTO();
             this.interrupt = null;
             this.sound = new SoundDTO();
         }
@@ -18,11 +16,10 @@ namespace PrimeMillionaire.Common.Data.DataStore
         public SaveDTO(SoundDTO sound)
         {
             this.uid = "";
-            this.progress = new ProgressDTO();
             this.interrupt = null;
             this.sound = sound;
         }
 
-        public SaveVO ToVO() => new(uid, progress.ToVO(), interrupt?.ToVO(), sound.ToVO());
+        public SaveVO ToVO() => new(uid, interrupt?.ToVO(), sound.ToVO());
     }
 }
