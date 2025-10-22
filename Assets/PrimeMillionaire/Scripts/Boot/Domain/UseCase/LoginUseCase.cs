578f29902ef1bbf7d02643ed2248acd3a4e63edd
kitatas <315kitatas10703@gmail.com>
2025-10-23 00:13:54 +0900

    fix: Login時にUserVOの保持

--- Assets/PrimeMillionaire/Scripts/Boot/Domain/UseCase/LoginUseCase.cs
+++ Assets/PrimeMillionaire/Scripts/Boot/Domain/UseCase/LoginUseCase.cs
@@ -2,35 +2,45 @@ using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using PrimeMillionaire.Common;
+using PrimeMillionaire.Common.Data.Entity;
 using PrimeMillionaire.Common.Domain.Repository;
 
 namespace PrimeMillionaire.Boot.Domain.UseCase
 {
     public sealed class LoginUseCase
     {
+        private readonly UserEntity _userEntity;
         private readonly PlayFabRepository _playFabRepository;
         private readonly SaveRepository _saveRepository;
 
-        public LoginUseCase(PlayFabRepository playFabRepository, SaveRepository saveRepository)
+        public LoginUseCase(UserEntity userEntity, PlayFabRepository playFabRepository, SaveRepository saveRepository)
         {
+            _userEntity = userEntity;
             _playFabRepository = playFabRepository;
             _saveRepository = saveRepository;
         }
 
         public async UniTask<bool> LoginAsync(CancellationToken token)
         {
+            var user = await GetLoginUserAsync(token);
+            _userEntity.Set(user);
+
+            return true;
+        }
+
+        private async UniTask<UserVO> GetLoginUserAsync(CancellationToken token)
+        {
             var saveData = _saveRepository.Load();
             if (saveData.isEmptyUid)
             {
                 var user = await CreateAsync(token);
                 _saveRepository.Save(user);
+                return user;
             }
             else
             {
-                var user = await _playFabRepository.LoginAsync(saveData.uid, token);
+                return await _playFabRepository.LoginAsync(saveData.uid, token);
             }
-
-            return true;
         }
 
         private async UniTask<UserVO> CreateAsync(CancellationToken token)
